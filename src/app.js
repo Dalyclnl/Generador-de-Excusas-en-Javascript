2a64743f91893534a0fb87c268fbb0f0a8411ec2
Dalyclnl <dalyarmijoa@yahoo.com>
2022-11-04 13:00:20 +0000

    Final commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#ebtn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generatexcuse();
+  });
+
+  function generatexcuse() {
+    var myActor = ["uron", "camello", "abuela", "raton", "sombra", "donNadie"];
+    var myAcc = ["desaparecio", "lavó", "quemó", "robó", "perdió", "tragó"];
+    var myWork = ["trabajo", "dibujo", "maqueta", "reporte", "investigacion"];
+
+    var who = myActor[Math.round(Math.random() * (myActor.length - 1))];
+    var did = myAcc[Math.round(Math.random() * (myAcc.length - 1))];
+    var what = myWork[Math.round(Math.random() * (myWork.length - 1))];
+
+    return ["<div>Mi " + who + " " + did + "<br> mi " + what + ".</div>"];
+  }
 };
